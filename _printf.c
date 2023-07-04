526abdd451c838b67537e590e0de5371a007a3dd
Oscar Guerra <oogl29@gmail.com>
2023-07-04 16:41:13 -0500

    Update files printf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#define BUFFER_SIZE 1024
+
 /**
  * _printf - Prints output according to a format
  * @format: The format string
@@ -29,51 +31,21 @@ int _printf(const char *format, ...)
             switch (format[i])
             {
                 case 'c':
-                    buffer_index = print_char(buffer, va_arg(args, int), buffer_index);
+                    buffer_index = _print_char(buffer, va_arg(args, int), buffer_index);
                     break;
                 case 's':
-                    buffer_index = print_string(buffer, va_arg(args, char *), buffer_index);
+                    buffer_index = _print_string(buffer, va_arg(args, char *), buffer_index);
                     break;
                 case '%':
-                    buffer_index = print_char(buffer, '%', buffer_index);
+                    buffer_index = _print_char(buffer, '%', buffer_index);
                     break;
-                case 'd':
-                case 'i':
-                    buffer_index = print_int(buffer, va_arg(args, int), buffer_index);
-                    break;
-                case 'u':
-                    buffer_index = print_unsigned(buffer, va_arg(args, unsigned int), 10, 0, buffer_index);
-                    break;
-                case 'o':
-                    buffer_index = print_unsigned(buffer, va_arg(args, unsigned int), 8, 0, buffer_index);
-                    break;
-                case 'x':
-                    buffer_index = print_unsigned(buffer, va_arg(args, unsigned int), 16, 0, buffer_index);
-                    break;
-                case 'X':
-                    buffer_index = print_unsigned(buffer, va_arg(args, unsigned int), 16, 1, buffer_index);
-                    break;
-                case 'b':
-                    buffer_index = print_unsigned(buffer, va_arg(args, unsigned int), 2, 0, buffer_index);
-                    break;
-                case 'S':
-                    buffer_index = print_string_custom(buffer, va_arg(args, char *), buffer_index);
-                    break;
-                case 'r':
-                    buffer_index = print_string_reverse(buffer, va_arg(args, char *), buffer_index);
-                    break;
-                case 'R':
-                    buffer_index = print_string_rot13(buffer, va_arg(args, char *), buffer_index);
-                    break;
-                default:
-                    buffer_index = print_char(buffer, '%', buffer_index);
-                    buffer_index = print_char(buffer, format[i], buffer_index);
-                    break;
+                // Rest of the cases
+                // ...
             }
         }
         else
         {
-            buffer_index = print_char(buffer, format[i], buffer_index);
+            buffer_index = _print_char(buffer, format[i], buffer_index);
         }
 
         if (buffer_index >= BUFFER_SIZE - 1)
